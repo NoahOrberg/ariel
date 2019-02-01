7da5ed556e03df0b3632a902555c0122a7a9c84d
NoahOrberg <noah.orberg.9999@gmail.com>
2019-02-01 15:41:17 +0900

    Initial Commit

---
+++ ariel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ariel
+//
+//  Created by Noah Orberg on 2019/02/01.
+//  Copyright © 2019 NoahOrberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
